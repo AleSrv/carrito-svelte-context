5c7ce3d6819bef481ddf70daaf8160c49b568704
alesrv <srvf2019@gmail.com>
2024-09-08 19:24:23 +0200

    primer comit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>Carrito compra con Context</h1>
+</main>
+
+<style>
+
+</style>
